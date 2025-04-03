a18710385703f8b3586e7b403b5c58841614fe5f
Anna Timofeeva <tiav@pkzdrav.ru>
2025-04-03 16:43:56 +0500

    #1 Set up an initial package structure, add maven wrapper

---
+++ src/main/java/ru/practicum/myblog/config/WebConfiguration.java
@@ -0,0 +1,11 @@
+package ru.practicum.myblog.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "ru.practicum.myblog")
+public class WebConfiguration {
+}
