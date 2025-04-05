b50eb5e346b56377d177aa1298d1c210111202a1
Anna Timofeeva <tiav@pkzdrav.ru>
2025-04-05 16:33:21 +0500

    #2 Add database configuration files and initial sql script

--- src/main/java/ru/practicum/myblog/config/WebConfiguration.java
+++ src/main/java/ru/practicum/myblog/config/WebConfiguration.java
@@ -2,10 +2,12 @@ package ru.practicum.myblog.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "ru.practicum.myblog")
+@PropertySource("classpath:config/application.properties")
 public class WebConfiguration {
 }
